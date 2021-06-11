977dc614f5bb589399d6d677aad6d2a552f1be9a
Pawel Kaminski <pawelkaminski@tipi.software>
2021-06-12 01:37:00 +0200

    Add increment functionality

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,28 +2,55 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement = (incrementBy) => {
+    this.setState({ count: this.state.count + incrementBy });
   };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={() => this.handleIncrement(1)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.renderTags()}
       </div>
     );
   }
 
   getBadgeClasses() {
-    let classes = 'badge m-2 badge-';
-    classes += (this.state.count === 0) ? 'warning' : 'primary';
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>No tags.</p>;
+    }
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
   }
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    return count === 0 ? "Zero" : count;
   }
-
-
 }
